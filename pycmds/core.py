8dcf3fc502abfa5ddfa862a2f3fa3f4a7c3eb886
Robert Russell <robertrussell.72001@gmail.com>
2018-09-13 07:35:59 -0700

    Added mutually exclusive option class.

--- pycmds/core.py
+++ pycmds/core.py
@@ -125,3 +125,22 @@ class AliasGroup(click.Group):
         :return: A sorted list of aliases.
         """
         return sorted(self.aliases)
+
+
+class MutuallyExclusiveOption(click.Option):
+
+    def __init__(self, *args, **kwargs):
+        self.mutually_exclusive = set(kwargs.pop("mutually_exclusive", []))
+        help = kwargs.get("help", "")
+        if self.mutually_exclusive:
+            ex_str = ", ".join(self.mutually_exclusive)
+            kwargs["help"] = help + " NOTE: This argument is mutually exclusive with arguments: [" + ex_str + "]."
+        super().__init__(*args, **kwargs)
+
+    def handle_parse_result(self, ctx, opts, args):
+        if self.mutually_exclusive.intersection(opts) and self.name in opts:
+            ctx.fail("Illegal usage: {!r} is mutually exclusive with arguments {!r}.".format(
+                self.name,
+                ", ".join(self.mutually_exclusive)
+            ))
+        return super().handle_parse_result(ctx, opts, args)
